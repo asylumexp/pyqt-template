d253fb02183946897095f1d36fe5d3ed42fc93b6
Sam Heinz <sam@samheinz.com>
2023-04-19 09:52:21 +1000

    fixing launch json and multiple screens

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from Ui_MainWindow import Ui_MainWindow
 
 
@@ -10,15 +10,15 @@ class MainWindow:
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.main_win)
 
-        self.ui.btn_go.clicked.connect(self.update_text)
-        a = QTextEdit
+        self.ui.btn_go_2.clicked.connect(self.update_text)
 
     def show(self):
         self.main_win.show()
 
     def update_text(self):
-        first = self.ui.firstNameInput.toPlainText()
-        self.ui.lbl_output.setText(f"You are {first}!")
+        first = self.ui.firstNameInput_2.toPlainText()
+        self.ui.lbl_output_2.setText(f"You are {first}!")
+        self.ui.stacked.setCurrentIndex(1)
 
 
 if __name__ == "__main__":
